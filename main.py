42fe518e8dcf64cd893a6a41c48782a8f732f2a1
craigdon-lee <craigdon.lee@deeeplabs.com>
2026-01-29 17:08:01 +0800

    Change origin

--- main.py
+++ main.py
@@ -37,11 +37,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "https://web-providend-ai-dev-qadujv.azurewebsites.net",
-    ],
+    allow_origins=os.getenv("CORS_ORIGINS"),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
